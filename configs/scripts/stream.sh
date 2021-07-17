5915ec6d77e9b04ed53b608e4ca8f5b3d7254b01
Andrew Peng <pengc99@gmail.com>
2021-07-16 22:01:28 -0500

    Delete configs directory

--- configs/scripts/stream.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while :
-do
-        ffmpeg -framerate 30 -video_size 800x600 -input_format mjpeg -i /dev/video0 -vcodec libx264 -b:v 1024k -r 30 -g 30 -profile:v baseline -pix_fmt yuv420p -preset veryfast -vf drawtext="fontsize=20: box=1: boxcolor=black@0.75: boxborderw=5: fontcolor=white: x=5: y=(h-text_h-5): text='%{localtime\:%m/%d/%Y - %T}'" -f flv rtmp://<hostname of your server>
-        sleep 5
-done
