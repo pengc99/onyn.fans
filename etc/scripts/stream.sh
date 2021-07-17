4c20f99719b84f9ab9cb18a3fa41a21e8faa74e8
Andrew Peng <pengc99@gmail.com>
2021-07-16 22:03:49 -0500

    Update stream.sh

--- etc/scripts/stream.sh
+++ etc/scripts/stream.sh
@@ -2,6 +2,6 @@
 
 while :
 do
-        ffmpeg -framerate 30 -video_size 800x600 -input_format mjpeg -i /dev/video0 -vcodec libx264 -b:v 1024k -r 30 -g 30 -profile:v baseline -pix_fmt yuv420p -preset veryfast -vf drawtext="fontsize=20: box=1: boxcolor=black@0.75: boxborderw=5: fontcolor=white: x=5: y=(h-text_h-5): text='%{localtime\:%m/%d/%Y - %T}'" -f flv rtmp://<hostname of your server>/live/<stream_key>
+        ffmpeg -framerate 30 -video_size 800x600 -input_format mjpeg -i /dev/video0 -vcodec libx264 -b:v 1024k -r 30 -g 30 -profile:v baseline -pix_fmt yuv420p -preset veryfast -vf drawtext="fontsize=20: box=1: boxcolor=black@0.75: boxborderw=5: fontcolor=white: x=5: y=(h-text_h-5): text='%{localtime\:%m/%d/%Y - %T}'" -f flv rtmp://<RTMP_host>/live/<stream_key>
         sleep 5
 done
